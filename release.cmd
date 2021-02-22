68b3bffd467946e433f56a70cf49513c17f1a141
JustOff <Off.Just.Off@gmail.com>
2021-02-22 16:39:55 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.0.6
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/location-4-evar-.+?\.xpi/download\/%VER%\/location-4-evar-%VER%\.xpi/" update.xml
+
+set XPI=location-4-evar-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE LICENSE.txt README.md *.cmd *.xpi *.exe
